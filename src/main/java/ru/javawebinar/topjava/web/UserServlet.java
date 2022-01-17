6f2d0e1648c02d16934de82b572308e03595b712
Kristina Oleinik <Vrednobear444@gmail.com>
2022-01-17 13:01:33 +0500

    Lesson1 Added slf4j,logback dependencies; Configuration file: logback.xml;

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,16 +1,22 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class UserServlet extends javax.servlet.http.HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doPost(HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-       request.getRequestDispatcher("/users.jsp").forward(request,response);
-     //   response.sendRedirect("/topjava/users.jsp");
+        log.debug("redirect to userList");
+        //request.getRequestDispatcher("/users.jsp").forward(request,response);
+     //  response.sendRedirect("/topjava/users.jsp");
         response.sendRedirect("users.jsp");
 }
 }
