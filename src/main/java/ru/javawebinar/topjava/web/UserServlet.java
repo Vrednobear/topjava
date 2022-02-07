a0cb869194657b44ef4c6ceeef846bad6b105166
Kristina Oleinik <Vrednobear444@gmail.com>
2022-02-07 22:22:07 +0500

    Lesson 3 Applied patch 3.7 User/Admin choice

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -3,20 +3,24 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UserServlet extends javax.servlet.http.HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
 
-    protected void doPost(HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        response.sendRedirect("meals");
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         log.debug("redirect to userList");
-        //request.getRequestDispatcher("/users.jsp").forward(request,response);
-     //  response.sendRedirect("/topjava/users.jsp");
         response.sendRedirect("users.jsp");
 }
 }
