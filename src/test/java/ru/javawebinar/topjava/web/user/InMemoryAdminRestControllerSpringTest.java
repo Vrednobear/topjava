dc7219cfd6e51f904de0f350179713d6d0bf575d
Kristina Oleinik <Vrednobear444@gmail.com>
2022-02-15 15:54:16 +0500

    Lesson 3 Added JUnit,Spring-test, javax-annotation-api dep. Created test directory and moved necessary files over there; Implemented two test classes(classic and Spring);

---
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,39 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private InMemoryUserRepository repository;
+
+    @Before
+    public void setUp(){
+        repository.init();
+    }
+
+    @Test
+    public void delete(){
+        controller.delete(UserTestData.USER_ID);
+        Assert.assertNull(repository.get(UserTestData.USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound(){
+        Assert.assertThrows(NotFoundException.class, () -> controller.delete(UserTestData.NOT_FOUND));
+    }
+}
