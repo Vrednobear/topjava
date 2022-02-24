f5ae71ff6ac838c4e5741661b755878bad95e5eb
Kristina Oleinik <Vrednobear444@gmail.com>
2022-02-25 02:30:26 +0500

    Lesson 3 Added Spring-jdbc functionality in project; Created tables;

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS USER_ROLES;
+DROP SEQUENCE IF EXISTS GLOBAL_SEQ;
+
+
+CREATE SEQUENCE GLOBAL_SEQ START 100000;
+
+CREATE TABLE USERS
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('GLOBAL_SEQ'),
+    name             VARCHAR                           NOT NULL,
+    email            VARCHAR                           NOT NULL,
+    password         VARCHAR                           NOT NULL,
+    registered       TIMESTAMP           DEFAULT now() NOT NULL,
+    enabled          BOOL                DEFAULT TRUE  NOT NULL,
+    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
+);
+
+CREATE UNIQUE INDEX unique_email ON USERS (email);
+
+CREATE TABLE USER_ROLES
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
